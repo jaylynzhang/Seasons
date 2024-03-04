c473d6513f6df4d8172bb8af5abfca4571e476ad
Yijia <60768920+Jasmine0814@users.noreply.github.com>
2024-03-04 01:45:00 -0800

    add tools in winter

---
+++ Assets/Scenes/Winter/PickStick.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickStick : MonoBehaviour
+{
+    public GameObject StickOnPlayer;
+    public GameObject PickUpText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StickOnPlayer.SetActive(false);
+        PickUpText.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            PickUpText.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                this.gameObject.SetActive(false);
+                StickOnPlayer.SetActive(true);
+                PickUpText.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PickUpText.SetActive(false);
+    }
+}
